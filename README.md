ebee1a8d9676fcba27b9d2d316995c4fd77cf402
grapeco <mihajlovs928@gmail.com>
2025-11-02 03:04:51 +0300

    Adding args.txt mechanic for more user friendly

---
+++ README.md
@@ -0,0 +1,21 @@
+# Requirements:
+1. **Rust**
+2. **yt-dlp**
+
+# How to use args.txt
+
+## For audio(example):
+```
+out - Path
+audio - Output file
+opus - Format
+```
+
+## For video(exampe):
+```
+out - Path
+video - Output file
+399+251(custom format, you can set this line to "default" or keep empty)
+```
+
+## You don't have to use this file, but it's more efficient then manualy type in cli. You always have a choise
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::{io, process::Command, time::Instant};
+use std::{
+    fs::File, io::{self, BufRead, BufReader}, process::Command
+};
 
 fn run_command(args: Vec<&str>) {
     Command::new(args[0])
@@ -7,17 +9,21 @@ fn run_command(args: Vec<&str>) {
         .expect(format!("Failed to execute command{:#?}", &args).as_str());
 }
 
-fn download_video(url: &str, format: &str, path: &str) {
-    let start_time = Instant::now();
-
+fn download_video(url: &str, format: Option<&str>, path: &str) {
     let mut args = vec![
         "yt-dlp",
-        "-f",
-        format,
         "-o",
         "%(title)s.%(ext)s",
         &url,
     ];
+
+    match format {
+        Some(value) => {
+            args.push("-f");
+            args.push(value);
+        }
+        None => {}
+    }
 
     if !path.is_empty() {
         args.push("-P");
@@ -25,13 +31,9 @@ fn download_video(url: &str, format: &str, path: &str) {
     }
 
     run_command(args);
-
-    println!("Video download complete in {:.2?}", start_time.elapsed());
 }
 
 fn download_audio(url: &str, format: &str, path: &str) {
-    let start_time = Instant::now();
-
     let mut args = vec![
         "yt-dlp",
         "-x",
@@ -48,45 +50,121 @@ fn download_audio(url: &str, format: &str, path: &str) {
     }
 
     run_command(args);
-
-    println!("Audio download complete in {:.2?}", start_time.elapsed());
 }
 
-fn main() {
-    let mut audio_format = String::new();
-    let mut video_format = String::new();
+fn args_in_file(file: &File) -> Vec<String> {
+    let reader = BufReader::new(file);
+    let mut input: Vec<String> = Vec::new();
 
-    let mut url = String::new();
-    println!("Enter video url(Youtube, Rutube, Vk video):");
-    io::stdin().read_line(&mut url).unwrap();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        input.push(line);
+    }
 
+    println!("{:?}", input);
+
+    input
+}
+
+fn is_file_empty(file: &File) -> bool {
+    let metadata = file.metadata().expect("File should have metadata");
+    metadata.len() == 0
+}
+
+fn manual_input(url: &str) {
     let mut path = String::new();
     println!("Enter path to install video(you can leave this field empty):");
     io::stdin().read_line(&mut path).unwrap();
 
-    let mut choise = String::new();
-    println!("What you want to install(video, audio):");
-    io::stdin().read_line(&mut choise).unwrap();
+    let mut output = String::new();
+    println!("Output file(video, audio):");
+    io::stdin().read_line(&mut output).unwrap();
 
-    match choise.trim() {
+    match output.trim() {
         "audio" => {
+            let mut audio_format = String::new();
             println!("Enter audio format(mp3, opus, m4a, wav, aac, alac, flac, vorbis):");
             io::stdin().read_line(&mut audio_format).unwrap();
 
             download_audio(&url, &audio_format, &path);
         } 
         "video" => {
-            run_command(vec![
-                "yt-dlp",
-                "-F",
-                &url,
-            ]);
+            let mut choise_format = String::new();
+            println!("Wanna see all formats for video or keep it default?(default or custom)");
+            io::stdin().read_line(&mut choise_format).unwrap();
 
-            println!("Enter video format code:");
-            io::stdin().read_line(&mut video_format).unwrap();
+            match choise_format.trim() {
+                "default" => {
+                    download_video(&url, None, &path);
+                }
+                "custom" => {
+                    run_command(vec![
+                        "yt-dlp",
+                        "-F",
+                        &url,
+                    ]);
 
-            download_video(&url, &video_format, &path);
+                    let mut video_format = String::new();
+                    println!("Enter video format code:");
+                    io::stdin().read_line(&mut video_format).unwrap();
+
+                    download_video(&url, Some(&video_format), &path);
+                }
+                _ => panic!("Wrong answer!!!")
+            }
         }
         _ => {}
+    }
+}
+
+fn file_input(url: &str, file: &File) {
+    let file_vec = args_in_file(&file);
+    let path = &file_vec[0];
+    let output = &file_vec[1];
+    
+    match output.trim() {
+        "audio" => {
+            let audio_format = &file_vec[2];
+            download_audio(&url, &audio_format, &path);
+        }
+        "video" => {
+            let video_format = file_vec.get(2);
+
+            match video_format {
+                Some(value) => {
+                    if value == "default" {
+                        download_video(url, None, path);
+                    } else {
+                        download_video(url, Some(value), &path);
+                    }
+                }
+                None => {
+                    run_command(vec![
+                        "yt-dlp",
+                        "-F",
+                        &url,
+                    ]);
+
+                    let mut format = String::new();
+                    io::stdin().read_line(&mut format).unwrap();
+
+                    download_video(url, Some(&format), &path);
+                }
+            }
+        }
+        _ => {}
+    }
+}
+
+fn main() {
+    let mut url = String::new();
+    println!("Enter video url(Youtube, Rutube, Vk video):");
+    io::stdin().read_line(&mut url).unwrap();
+
+    let file = File::open("args.txt").expect("Could not open file");
+
+    match is_file_empty(&file) {
+        true => manual_input(&url),
+        false => file_input(&url, &file),
     }
 }
